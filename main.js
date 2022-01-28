e09127c8faea79b07161fa84dfe00682378274dd
Roman <vizionssmith@gmail.com>
2022-01-28 15:26:48 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
 let textFile = document.querySelector("#textFile");
 let form  = document.querySelector("form");
+let fileList = document.querySelector("#fileList");
 
 
 form.addEventListener("submit", event => {
     event.preventDefault();
-    let a = textFile.textContent;
-    fetch("file.txt", {
-        method: 'PUT', // Method itself
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8' // Indicates the content
-        },
-        body: JSON.stringify(a) // We send data in JSON format
-    })
-        .then(res => res.text()) // or res.json()
-        .then(res => console.log(res))
 
+    fetch(fileList.value, {
+        method: 'PUT',
+        body: textFile.value
+    })
 })
 
-    fetch("file.txt", {method: "GET"})
-        .then(
-            function (response) {
-                if (!response.ok) {
-                    console.log("error" + response.status);
-                }
 
-                response.text().then(data => {
-                    textFile.textContent = data;
-                    // console.log(textFile.textContent)
-                });
-                console.log(textFile.textContent)
+
+    fetch("/")
+        .then(res => res.text().then(files => {
+            for(let file of files.split("\n")){
+                let option = document.createElement("option");
+                option.textContent = file;
+                fileList.appendChild(option);
             }
-        )
+            loadCurrentFile()
+        }))
+
         .catch(err => {
             if (err) console.log('Fetch Error :-S', err);
         })
+
+
+
+function loadCurrentFile(){
+    fetch(fileList.value).then(res => res.text().then(data => textFile.textContent = data));
+}
+
+
+
+fileList.addEventListener("change", loadCurrentFile);
 
